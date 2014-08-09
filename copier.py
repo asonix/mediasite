36009808af53c18627b2725ca4b54b18bea13ac0
asonix <asonix.dev@gmail.com>
2014-08-09 01:36:29 -0500

    added important thing to copier

--- copier.py
+++ copier.py
@@ -30,6 +30,8 @@ os.chdir ('/home/riley/site')
 for indexfile in  locate ('index.html'):
     contents = open (indexfile, 'r').readlines ()
     contents = find_and_replace ('index.html','',contents)
+    # contents = find_and_replace ('colors_pastel.css','colors.css',contents)
+    contents = find_and_replace ('colors.css','colors_pastel.css',contents)
     f = open (indexfile, 'w')
     f.truncate ()
     f.writelines (contents)
